40027ada5d1a5a227c90edc977ba3b7ba4e59e57
Leon Do <leondo123@gmail.com>
2017-01-21 21:00:17 -0500

    heroku again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded ({ extended:true }));
+app.use(bodyParser.urlencoded ({ extended:true }))
 
 
 app.get('/', function (req, res) {
